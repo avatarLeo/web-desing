f60857b628e2a581438e708ec11a839b5ee1d295
avatarLeo <leandroalmeida055@gmail.com>
2024-09-24 13:17:19 -0300

    Aula dia 24-09-24

--- javascript/objeto/aula.js
+++ javascript/objeto/aula.js
@@ -1,24 +1,37 @@
-function Carro(velocidadeMaxima = 200, delta = 5){
-    let velocidadeAtual = 0
-    this.acelerar = function(){
-        if(velocidadeAtual + delta <= velocidadeMaxima){
-            velocidadeAtual += delta
-        }else{
-            velocidadeAtual = velocidadeMaxima
-        }
-    }
 
-    this.getVelocidade = function (){
-        return velocidadeAtual
+
+
+const lista = [
+    {marca:"BMW", velMax: 200, cor: 'Azul'},
+    {marca:"Ferrari", velMax: 320, cor: 'Vermelho'},
+    {marca:"Fusca", velMax: 180, cor: 'Rosa'},
+    {marca:"Uno", velMax: 200, cor: 'Prata'},
+]
+
+const produtos = [
+    {nome: "Margarina", valor: 2},
+    {nome: "Frango", valor: 18},
+    {nome: "Macarrão", valor: 3},
+    {nome: "Feijão", valor: 11},
+    {nome: "Farinha de trigo", valor: 8},
+]
+
+let carroVeloz = 0
+let velo = 0
+for(i in lista){
+    let carro = lista[i]
+    if (carro.velMax > velo){
+        carroVeloz = carro
+        velo = carro.velMax
     }
 }
+let produtoCaro = {}
+let valorAtual = 0
+produtos.forEach(p => {
+    if(p.valor > valorAtual){
+        produtoCaro = p
+        valorAtual = p.valor
+    }
+})
 
-const uno = new Carro
-const ferrari = new Carro(340, 20)
-
-uno.acelerar()
-uno.acelerar()
-
-ferrari.acelerar()
-console.log( typeof uno)
-console.log( typeof Carro)
+console.log(produtoCaro.nome, produtoCaro.valor)
